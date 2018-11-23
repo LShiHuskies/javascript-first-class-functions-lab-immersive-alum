87d2a50d5976dedbdae06b2ecbaa001fa0275a14
github username <github email address>
2018-11-23 02:21:14 -0500

    doubler and tripler

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function countdown(callback) {
   return window.setTimeout(callback, 2000);
 };
+
+function createMultiplier(multiplierValue) {
+  return function (value) {
+    return multiplierValue * value
+  }
+}
+
+function multipler(a,b) {
+  return a * b;
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doubler = multipler.bind(null, 2);
+var tripler = multipler.bind(null, 3);
